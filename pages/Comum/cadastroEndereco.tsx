85a17d3bccfeae25ccd6d66855b87d6fd163be71
Murilo Guilherme Lopes <by_murilo@hotmail.com>
2020-09-23 22:42:57 -0300

    Adicionando campos tela cadastro endereço

--- pages/Comum/cadastroEndereco.tsx
+++ pages/Comum/cadastroEndereco.tsx
@@ -3,15 +3,14 @@ import { useNavigation } from '@react-navigation/native';
 import {
   StyleSheet, View, KeyboardAvoidingView, Platform,  
 } from 'react-native';
-import { Appbar, TextInput, DefaultTheme, Chip, Text } from 'react-native-paper';
+import { Appbar, TextInput, DefaultTheme, Chip, Text, Snackbar } from 'react-native-paper';
 import { TextInputMask } from 'react-native-masked-text'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { RectButton } from 'react-native-gesture-handler';
 
 /* 
 ToDos:
-- Botão Salvar
 - Trazer dados reais/consistir
-- Campos numéricos nao tem botão 'fechar' no teclado
-- Quando está nos campos de baixo o teclado fica por cima
 */
 
 const cadastroEndereco = () => {
@@ -21,85 +20,20 @@ const cadastroEndereco = () => {
     navigation.goBack();
   };
 
-  const [nome, setNome] = React.useState('');
-  const [sobrenome, setSobrenome] = React.useState('');
-  const [genero, setGenero] = React.useState('');
-  const [CPF, setCPF] = React.useState('');  
-  const [whatsapp, setWhatsapp] = React.useState('');
-  const [dataNascimento, setDataNascimento] = React.useState('');
-  const [email, setEmail] = React.useState('');
+  const [CEP, setCEP] = React.useState('');
+  const [endereco, setEndereco] = React.useState('');
+  const [numero, setNumero] = React.useState('');
+  const [complemento, setComplemento] = React.useState('');  
+  const [bairro, setBairro] = React.useState('');
+  const [cidade, setCidade] = React.useState('');
+  const [estado, setEstado] = React.useState('');
 
-  const [tamanhoFonteGenero, setTamanhoFonteGenero] = React.useState(0);
-  const [genMascSelecionado, setGenMascSelecionado] = React.useState(false);
-  const [genFemSelecionado, setGenFemSelecionado] = React.useState(false);
-  const [genOutrosSelecionado, setGenOutrosSelecionado] = React.useState(false);
-  const [colorGenMasc, setColorGenMasc] = React.useState('black');
-  const [colorGenFem, setColorGenFem] = React.useState('black');
-  const [colorGenOutros, setColorGenOutros] = React.useState('black');
+  const [snackSalvarVisible, setSnackSalvarVisible] = React.useState(false);
+  const onToggleSnackSalvar = () => setSnackSalvarVisible(!snackSalvarVisible);
 
-  const handleSelecaoGenero = (genero: string) => {
-    if(genero.includes('Masculino'))
-    {
-      //faz a selecao da cor antes pra nao sofrer o efeito async do useState
-      if(genMascSelecionado == true){
-        setColorGenMasc('black')
-        setTamanhoFonteGenero(16)
-      }else{
-        setColorGenMasc('blue')
-        setTamanhoFonteGenero(12)
-      }      
-
-      setGenMascSelecionado(!genMascSelecionado)      
-
-      setGenFemSelecionado(false)
-      setColorGenFem('black')
-
-      setGenOutrosSelecionado(false)
-      setColorGenOutros('black')
-      
-    }
-    else if(genero.includes('Feminino'))
-    {
-      //faz a selecao da cor antes pra nao sofrer o efeito async do useState
-      if(genFemSelecionado == true){
-        setColorGenFem('black')
-        setTamanhoFonteGenero(16)
-      }else{
-        setColorGenFem('blue')
-        setTamanhoFonteGenero(12)
-      }     
-
-      setGenFemSelecionado(!genFemSelecionado)      
-
-      setGenMascSelecionado(false)
-      setColorGenMasc('black')
-      
-      setGenOutrosSelecionado(false)
-      setColorGenOutros('black')      
-    }
-    else if(genero.includes('Outros'))
-    { 
-      //faz a selecao da cor antes pra nao sofrer o efeito async do useState
-      if(genOutrosSelecionado == true){
-        setColorGenOutros('black')
-        setTamanhoFonteGenero(16)
-      }else{
-        setColorGenOutros('blue')
-        setTamanhoFonteGenero(12)
-      }    
-
-      setGenOutrosSelecionado(!genOutrosSelecionado)
-
-      setGenMascSelecionado(false)
-      setColorGenMasc('black')
-
-      setGenFemSelecionado(false)
-      setColorGenFem('black')
-    }
-  }
 
   React.useEffect(() => {
-    setTamanhoFonteGenero(16)
+
   }, [])
 
   const theme = {
@@ -118,56 +52,40 @@ const cadastroEndereco = () => {
   };
 
   return (
-    <KeyboardAvoidingView
+    <KeyboardAwareScrollView
       style={styles.container_principal}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
       <Appbar.Header statusBarHeight={-15} style={{ height: 45, backgroundColor: '#212F3C' }}>
         <Appbar.Action icon="arrow-left-circle" size={30} onPress={_goBack} />
         <Appbar.Content title="Endereço" />
       </Appbar.Header>
 
-      <TextInput theme={theme} label="Nome" value={nome} onChangeText={text => setNome(text)}/>
-      <TextInput theme={theme} label="Sobrenome" value={sobrenome} onChangeText={text => setSobrenome(text)}/>
-      <Text  style={{color: '#A79898', marginStart: 12, marginTop: 10, marginBottom: 10, fontSize: tamanhoFonteGenero}}>Gênero</Text>
-      <View style={{flexDirection: 'row'}}>
-        <Chip selected={genMascSelecionado} selectedColor={colorGenMasc} onPress={() => {handleSelecaoGenero('Masculino')}} style={{width: 110, marginStart: 10, marginEnd: 10}}>Masculino</Chip>
-        <Chip selected={genFemSelecionado} selectedColor={colorGenFem} onPress={() => {handleSelecaoGenero('Feminino')}} style={{width: 100, marginEnd: 10}}>Feminino</Chip>
-        <Chip selected={genOutrosSelecionado} selectedColor={colorGenOutros} onPress={() => {handleSelecaoGenero('Outros')}} style={{width: 100}}>Outros</Chip>
-      </View> 
-      <TextInput theme={theme} label="CPF" value={CPF}
-        render={props =><TextInputMask
-          {...props}
-          type={'cpf'}
-          value={CPF}
-          onChangeText={text => setCPF(text)}
-        />}
-      />
-      <TextInput theme={theme} label="WhatsApp/Celular" value={whatsapp}
-        render={props =><TextInputMask
-          {...props}
-          type={'cel-phone'}
-          options={{
-            maskType: 'BRL',
-            withDDD: true,
-            dddMask: '(99) '
-          }}
-          value={whatsapp}
-          onChangeText={text => setWhatsapp(text)}
-        />}
-      />
-      <TextInput theme={theme} label="Data de Nascimento" value={dataNascimento}
-        render={props =><TextInputMask
-          {...props}
-          type={'datetime'}
-          options={{format: 'DD/MM/YYYY'}}
-          value={dataNascimento}
-          onChangeText={text => setDataNascimento(text)}
-        />}
-      />
-      <TextInput theme={theme} label="Email" value={email} onChangeText={text => setEmail(text)}/>
-      
-    </KeyboardAvoidingView>
+      <TextInput theme={theme} label="CEP" value={CEP} onChangeText={text => setCEP(text)}/>
+      <TextInput theme={theme} label="Endereço" value={endereco} onChangeText={text => setEndereco(text)}/>
+      <TextInput theme={theme} label="Número" value={numero} onChangeText={text => setNumero(text)}/>
+      <TextInput theme={theme} label="Complemento" value={complemento} onChangeText={text => setComplemento(text)}/>
+      <TextInput theme={theme} label="Bairro" value={bairro} onChangeText={text => setBairro(text)}/>
+      <TextInput theme={theme} label="Cidade" value={cidade} onChangeText={text => setCidade(text)}/>
+      <TextInput theme={theme} label="Estado" value={estado} onChangeText={text => setEstado(text)}/>
+
+
+      <View style={styles.buttonView}>
+        <RectButton style={styles.button} onPress={onToggleSnackSalvar}>
+          <Text style={styles.buttonText}>Salvar</Text>
+        </RectButton>
+      </View>
+
+      <Snackbar
+        visible={snackSalvarVisible}
+        onDismiss={onToggleSnackSalvar}
+        action={{
+          label: 'Ok',
+          onPress: _goBack,
+        }}>
+        Dados salvos com sucesso!
+      </Snackbar>
+        
+    </KeyboardAwareScrollView>
   );
 };
 
@@ -180,5 +98,26 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     paddingTop: 40,
   },
-  
+
+  buttonView: {
+    margin: 15,
+    alignItems: 'center'
+  },
+
+  button: {
+    backgroundColor: '#0081DA',
+    height: 45,
+    width: '50%',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+  },
+
+  buttonText: {
+    flex: 1,
+    color: '#FFF',
+    fontSize: 20,
+  },
 });
